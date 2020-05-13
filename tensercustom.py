ed49dfb5217e4aab206909d43a443716acea24bb
CookIsGood <Morkovo4ka513@yandex.ru>
2020-05-13 19:48:30 +0300

    Добавлена перетасовка train/test_data и слой conv1D

--- tensercustom.py
+++ tensercustom.py
@@ -8,20 +8,21 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-training_img = np.load('train_img.npy')  # loading
-training_label = np.load('train_label.npy')
 
 test_img = np.load('test_img.npy')  # loading
 test_label = np.load('test_label.npy')
 
-
+c = np.c_[test_img.reshape(len(test_img), -1), test_label.reshape(len(test_label), -1)]
+np.random.shuffle(c)
+shuffle_test_img = c[:, :test_img.size//len(test_img)].reshape(test_img.shape)
+shuffle_test_label = c[:, test_img.size//len(test_img):].reshape(test_label.shape)
 
 class_names = ['Yes', 'No']
 
 model = keras.models.load_model('tensorblack.h5')
-test_lose, test_acc = model.evaluate(test_img, test_label)
+test_lose, test_acc = model.evaluate(shuffle_test_img, shuffle_test_label)
 print("\nТочность на проверочных данных: ", test_acc)
-predictions = model.predict(test_img)
+predictions = model.predict(shuffle_test_img)
 print(predictions[0])
 print(np.argmax(predictions[0]))
 k = 0
@@ -31,7 +32,7 @@ for i in range(2175, 2200):
     k = k + 1
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(test_img[i], cmap=plt.cm.binary)
+    plt.imshow(shuffle_test_img[i], cmap=plt.cm.binary)
     plt.grid(False)
-    plt.xlabel('c:' + class_names[int(np.argmax(predictions[i]))] + '/n:' + class_names[test_label[i]])
+    plt.xlabel('c:' + class_names[int(np.argmax(predictions[i]))] + '/n:' + class_names[shuffle_test_label[i]])
 plt.show()
--- testcustom.py
+++ testcustom.py
@@ -1,19 +1,17 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 # TensorFlow и tf.keras
-import tensorflow as tf
 from tensorflow import keras
 
 # Вспомогательные библиотеки
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import cv2
 
 # Создание тренировачных входных данных
-DATADIR = "A:\python\hatetensorflow\img"
+DATADIR = "A:\python\createcustomtensorflow\img"
 CATEGORIES = ["True", "False"]
-IMG_SIZE = 600
+IMG_SIZE = 300
 training_data = []
 
 
@@ -41,10 +39,18 @@ np.save('train_img.npy', training_img)  # saving
 training_img = np.load('train_img.npy')  # loading
 np.save('train_label.npy', training_label)
 training_label = np.load('train_label.npy')
-print("Размер и количество тренировочных картинок", training_img.shape)
-print("Количество тренировочных меток", len(training_label))
+
+c1 = np.c_[training_img.reshape(len(training_img), -1), training_label.reshape(len(training_label), -1)]
+np.random.shuffle(c1)
+shuffle_training_img = c1[:, :training_img.size//len(training_img)].reshape(training_img.shape)
+shuffle_training_label = c1[:, training_img.size//len(training_img):].reshape(training_label.shape)
+np.save('train_img.npy', shuffle_training_img)
+
+
+print("Размер и количество тренировочных картинок", shuffle_training_img.shape)
+print("Количество тренировочных меток", len(shuffle_training_label))
 # Создание тестовых входных данных
-TESTDATADIR = "A:\python\hatetensorflow\img1"
+TESTDATADIR = "A:\python\createcustomtensorflow\img1"
 TESTCATEGORIES = ["True", "False"]
 test_data = []
 
@@ -76,10 +82,17 @@ test_label = np.load('test_label.npy')
 # training_img = training_img / 255
 # test_img = test_img / 255
 
-print("Размер и количество тестовых картинок", test_img.shape)
-print("Количество тестовых меток", len(test_label))
+c = np.c_[test_img.reshape(len(test_img), -1), test_label.reshape(len(test_label), -1)]
+np.random.shuffle(c)
+shuffle_test_img = c[:, :test_img.size//len(test_img)].reshape(test_img.shape)
+shuffle_test_label = c[:, test_img.size//len(test_img):].reshape(test_label.shape)
 
+
+print("Размер и количество тестовых картинок", shuffle_test_img.shape)
+print("Количество тестовых меток", len(shuffle_test_label))
+input_shape = (IMG_SIZE, IMG_SIZE)
 model3 = keras.Sequential([
+    keras.layers.Conv1D(3, 1, padding="same", activation="relu", input_shape=input_shape),
     keras.layers.Flatten(input_shape=(IMG_SIZE, IMG_SIZE)),
     keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(2, activation='softmax')
@@ -87,8 +100,8 @@ model3 = keras.Sequential([
 model3.compile(optimizer='Adadelta',
                loss='sparse_categorical_crossentropy',
                metrics=['accuracy'])
-model3.fit(training_img, training_label, epochs=10)
-test_lose, test_acc = model3.evaluate(test_img, test_label)
+model3.fit(shuffle_training_img, shuffle_training_label, epochs=10)
+test_lose, test_acc = model3.evaluate(shuffle_test_img, shuffle_test_label)
 print("\nТочность на проверочных данных: ", test_acc)
 model3.save('tensorblack.h5')
 print('Модель успешно сохранена')
